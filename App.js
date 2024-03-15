4d5e1040796746e5c1ac142cc472106cc72cd22f
jcxpjau <jcxpjau@live.com>
2024-03-15 10:09:33 -0300

    Inicio do Projeto com menus

---
+++ App.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './src/Home';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={Home} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
